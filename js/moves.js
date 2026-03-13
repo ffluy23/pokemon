aac90e63c9f14f44d1290027e76360d7480c2402
ffluy23 <ffluy23@gmail.com>
2026-03-13 13:43:47 +0900

    Create moves.js

---
+++ js/moves.js
@@ -0,0 +1,39 @@
+export const moves = {
+
+  번개펀치: {
+    power: 40,
+    type: "전기",
+    effect: {
+      chance: 0.1,
+      status: "화상"
+    }
+  },
+
+  "10만볼트": {
+    power: 40,
+    type: "전기",
+    effect: {
+      chance: 0.3,
+      status: "풀죽음"
+    }
+  },
+
+  전기쇼크: {
+    power: 40,
+    type: "얼음",
+    effect: {
+      chance: 0.1,
+      status: "전기"
+    }
+  },
+
+  방전: {
+    power: 40,
+    type: "전기",
+    effect: {
+      chance: 0.3,
+      status: "풀죽음"
+    }
+  }
+
+};
